c4b39c1ec008a496e9d74dfbfb260bfa481e131f
Karoffski <50497779+Karoffski@users.noreply.github.com>
2022-03-19 01:49:51 +0100

    Update Search_Z.js

--- Project/Function/Search_Z/Search_Z.js
+++ Project/Function/Search_Z/Search_Z.js
@@ -1,12 +1,17 @@
 function search_z() {
+    
+    // Input utilisateur
     var x = document.getElementById("takeZ").value;
+    // Caractères en minuscule
     var y = x.toLowerCase()
 
     var z = (y.match(/z/g) || []).length;
 
     if (z > 0){
+        // Output si un ou plusieurs z
         return document.getElementById("returnZ").innerHTML = "Il y a " + z + " z";
     } else{
+        // Output si 0 z
         return document.getElementById("returnZ").innerHTML = "Il y a 0 z";
     }
 }
