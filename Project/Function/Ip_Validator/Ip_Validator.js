fbc006abf06900d54300c672b59fbe1f844156c2
Karoffski <50497779+Karoffski@users.noreply.github.com>
2022-03-19 01:45:24 +0100

    Update Ip_Validator.js

--- Project/Function/Ip_Validator/Ip_Validator.js
+++ Project/Function/Ip_Validator/Ip_Validator.js
@@ -1,9 +1,14 @@
 function ip_validator() {
+    // Input utilisateur
     var x = document.getElementById("takeIP").value;
-
+    
     if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(x)) {  
+        
+        // Output si valide
         return document.getElementById("returnIP").innerHTML = "Valid";
     } else {
+        
+        // Output si invalide
         return document.getElementById("returnIP").innerHTML = "Not valid";
     }
 }
