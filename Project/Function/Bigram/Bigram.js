ddf651ed6daa5c119423a8c36755d2a918935940
Karoffski <50497779+Karoffski@users.noreply.github.com>
2022-03-19 01:56:11 +0100

    Update Bigram.js

--- Project/Function/Bigram/Bigram.js
+++ Project/Function/Bigram/Bigram.js
@@ -1,9 +1,13 @@
 function bigram(data, length) {
-                                    
-    var data = document.getElementById("takeBigram").value.split(" ");
-    var length = 3
-    var result = [];
     
+    // Input utilisateur                                
+    var data = document.getElementById("takeBigram").value.split(" ");
+    
+    // Initialisation des paramètres
+    var length = 3
+    // Initialisation du tableau
+    var result = [];
+   
         for (var i = 0; i < data.length - (length - 1); i++) {
             var progress = [];
     
@@ -13,9 +17,11 @@ function bigram(data, length) {
     
             result.push(progress);
         }
-
-        var stringResult = result.toString()
-        var finalResult = stringResult.replace(/,/g, ' ');
         
+        // Transformer le résultat sous forme d'une chaine de caractères
+        var stringResult = result.toString()
+        // Remplacer les , par un espace
+        var finalResult = stringResult.replace(/,/g, ' ');
+        // Output utilisateur
         return document.getElementById("returnBigram").innerHTML = finalResult;
 }
